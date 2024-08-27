09940c4c8f2ed5987fe1a387c1cdac5986148021
tanisha1111 <106333535+tanisha1111@users.noreply.github.com>
2024-08-27 17:55:17 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 14533
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jamshedpur"
+let accountState;
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+console.log(3+3)
+console.log("Tanisha")
+
+let name = "Tanisha" //string
+let age = 21 //int
+let isLoggedIn = false //boolean
+
+console.log (typeof "Tanisha");
+console.log (typeof age);
+console.log (typeof null);
+console.log (typeof undefined);
