d77796b0c11fc0766dd33d0586740e4dbef329ee
tanisha1111 <106333535+tanisha1111@users.noreply.github.com>
2024-08-28 09:45:17 +0000

    container config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,46 @@
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+let value = 3
+let negValue = -value
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3); */
+
+let str1 = "Hello"
+let str2 = "Tanisha"
+
+let str3 = str1 + str2
+console.log(str3);
+
+/*console.log("1"+2);
+console.log(1 +"2");
+console.log("1"+2+2);
+console.log(1+2+"2"); //dont write codes like these  */
+
+console.log(+true);
+console.log(+"");
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
